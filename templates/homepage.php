7f6f8cf0993a0d87126d4535f718781ac17e6270
Dimitris Kotios <dimitris.kotios@gmail.com>
2015-10-22 09:39:32 +0200

    Commit Ueber Museum

--- templates/homepage.php
+++ templates/homepage.php
@@ -55,10 +55,11 @@
         </div>
         <div class="row">
             <div class="col-lg-4 col-lg-offset-2">
-                <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
+                <p>Entdecken Sie in im virtuellen Museum die größten Informatiker und Informatikerinnen aller Zeiten. Hier erfahren Sie welche bahnbrechenden Erfindungen in Geschichte und Gegenwart das Computerzeitalter geprägt haben. 
+                </p>
             </div>
             <div class="col-lg-4">
-                <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
+                <p>In dieser Online Bibliothek erhalten Sie kostenlose biographische Angaben. Das virtuelle Museum ist ein Projekt der DHBW Stuttgart in Kooperation mit den Studierenden der Angewandten Informatik.</p>
             </div>
             <div class="col-lg-8 col-lg-offset-2 text-center">
                 <a href="./?action=allPersons" class="btn btn-lg btn-outline">
